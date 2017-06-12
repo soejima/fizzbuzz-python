bea1a4755c4a77e04f112c4e765fc4938711f524
Henrique Soejima <soejima@gmail.com>
2017-06-12 14:45:35 -0300

    first failing test

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -1,4 +1,10 @@
 import unittest
+from fizzbuzz import FizzBuzz
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def test_FizzBuzz(self):
+        self.assertEqual(FizzBuzz(1), 1)
 
 if __name__ == '__main__':
     unittest.main()
