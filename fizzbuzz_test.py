fbd7b39fb0a1519a41e5809bcb66b3f84aa3c93f
Henrique Soejima <soejima@gmail.com>
2017-06-12 14:47:45 -0300

    second failing test

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,6 +5,7 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_FizzBuzz(self):
         self.assertEqual(FizzBuzz(1), 1)
+        self.assertEqual(FizzBuzz(2), 2)
 
 if __name__ == '__main__':
     unittest.main()
