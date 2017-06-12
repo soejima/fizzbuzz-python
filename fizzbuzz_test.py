24997820db3dc942194d02556e73de87f5f136ad
Henrique Soejima <soejima@gmail.com>
2017-06-12 15:15:14 -0300

    grouping tests about multiples of 3

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,16 +5,20 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_FizzBuzz_1(self):
         self.assertEqual(FizzBuzz(1), 1)
+
     def test_FizzBuzz_2(self):
         self.assertEqual(FizzBuzz(2), 2)
-    def test_FizzBuzz_3(self):
+
+    def test_FizzBuzz_multiples_of_3(self):
         self.assertEqual(FizzBuzz(3), "Fizz")
+        self.assertEqual(FizzBuzz(9), "Fizz")
+
     def test_FizzBuzz_5(self):
         self.assertEqual(FizzBuzz(5), "Buzz")
+
     def test_FizzBuzz_15(self):
         self.assertEqual(FizzBuzz(15), "FizzBuzz")
-    def test_FizzBuzz_9(self):
-        self.assertEqual(FizzBuzz(9), "Fizz")
+
     def test_FizzBuzz_10(self):
         self.assertEqual(FizzBuzz(10), "Buzz")
 
