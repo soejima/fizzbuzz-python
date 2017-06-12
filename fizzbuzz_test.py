5e13b238bdc78eca46ed985a1a30f78bb175b8a9
Henrique Soejima <soejima@gmail.com>
2017-06-12 14:54:23 -0300

    third failing test

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -7,6 +7,8 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(FizzBuzz(1), 1)
     def test_FizzBuzz_2(self):
         self.assertEqual(FizzBuzz(2), 2)
+    def test_FizzBuzz_3(self):
+        self.assertEqual(FizzBuzz(3), "Fizz")
 
 if __name__ == '__main__':
     unittest.main()
